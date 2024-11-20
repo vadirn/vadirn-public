5794a8995f712cd14402a36beba7ad01652c9f4d
Vadim K. <vadim@vadirn.io>
2024-11-20 15:05:51 +0500

    chore: note path utils

--- apps/web/src/lib/cache/notes.ts
+++ apps/web/src/lib/cache/notes.ts
@@ -1,9 +1,13 @@
+import { toKebabCase } from '@workspace/standard/string';
+import { get } from 'svelte/store';
 import { app } from '$lib/paths/app';
+import { page } from '$app/stores';
 
-export const note = (title: string) => app.notes(title).toString();
+export const draftNotes = [
+	'Core?',
+];
 
 export const notes = [
-	'Core?',
 	'Challenges of Server Data Representation on the Client: Collections',
 	'Typesafe API Routes: Methods, Expected Responses, and More',
 	'Achieving Typesafe Routes Through Proxy Objects',
@@ -14,5 +18,23 @@ export const notes = [
 	'The First Step to a Better Developer Experience',
 	'State Machines for the Rest of Us',
 	'Awesome Pull Requests',
-	'Conventional Commits and Reasons to Change Code',
+	'Conventional Commits and Reasons for Code Change',
 ];
+
+const notesMap = new Map(
+	notes.map(title => [toKebabCase(title), title]),
+);
+
+export function getNoteTitle(slug: string) {
+	return notesMap.get(slug);
+}
+
+export function note(title: string) {
+	return app.notes(title).toString();
+}
+
+export function getCurrentNoteTitle() {
+	const slug = get(page).url.pathname.split('/').pop();
+
+	return notesMap.get(slug);
+}
