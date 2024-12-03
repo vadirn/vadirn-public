94a3d2cd77b3c45ac4a6a73f278b520af943eb0e
Vadim K. <vadim@vadirn.io>
2024-12-03 20:02:22 +0500

    chore: more ui components and actions

---
+++ ui/components/src/overlays/Modal/Modal.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { Layers, portal } from '@ui/actions/portal';
+
+	const { children } = $props();
+</script>
+
+<div class="modal" use:portal={Layers.Modals}>
+	{@render children()}
+</div>
---
+++ ui/components/src/utils/Portal/Portal.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { portal } from '@ui/actions/portal';
+
+	const { children, className = '' } = $props();
+</script>
+
+<div class={className} use:portal>
+	{@render children()}
+</div>
