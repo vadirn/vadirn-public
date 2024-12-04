f4cd5ffe4fe8d595db7c845fd1b16c419d7fc811
Vadim Kotov <vadim@vadirn.io>
2024-12-04 14:14:10 +0500

    feat: better modal

--- ui/components/src/overlays/Modal/Modal.svelte
+++ ui/components/src/overlays/Modal/Modal.svelte
@@ -1,10 +1,32 @@
 <script lang="ts">
 	import { Layers, portal } from '@ui/actions/portal';
 	import { shortcuts } from '@libs/shortcuts';
+	import { assertFalse } from '@libs/standard/error';
+	import { getFocusableElements } from '@libs/standard/dom';
+	import { isNil, notNil } from '@libs/standard/common';
+	import type { Snippet } from 'svelte';
+	import type { Fn } from '@libs/standard/function';
 
-	// https://github.com/sveltejs/eslint-plugin-svelte/issues/818
-	let { children, trigger, isVisible = $bindable(false) } = $props();
+	// https://github.com/svelftejs/eslint-plugin-svelte/issues/818
+	let {
+		children,
+		trigger,
+		isBrowser,
+		isVisible = $bindable(false),
+	}: {
+		children: Snippet;
+		trigger?: Snippet<[Fn<void, [Event, boolean]>]>;
+		isBrowser?: boolean;
+		isVisible?: boolean;
+	} = $props();
+
 	let element: HTMLElement | undefined = $state();
+
+	assertFalse(
+		isVisible && isBrowser === undefined,
+		'The isBrowser property should be set when '
+			+ 'creating a modal with isVisible = true',
+	);
 
 	function setVisibility(event: Event, value: boolean) {
 		// prevent bubbling, so that $effect doesn't take over the click
@@ -19,27 +41,79 @@
 	};
 
 	const onkeydown = shortcuts({
-		esc: () => {
+		'esc': () => {
 			isVisible = false;
+		},
+		'tab': (event) => {
+			if (isNil(element)) return;
+			const activeElement = document.activeElement as HTMLElement;
+
+			const focusableElements = getFocusableElements(element);
+			const firstFocusableElement = focusableElements[0];
+			const lastFocusableElement = focusableElements[
+				focusableElements.length - 1
+			];
+
+			if (!element.contains(activeElement)) {
+				event.preventDefault();
+				if (notNil(firstFocusableElement)) {
+					firstFocusableElement.focus();
+
+					return;
+				}
+				activeElement?.blur();
+			}
+
+			if (activeElement !== lastFocusableElement) return;
+
+			event.preventDefault();
+			firstFocusableElement?.focus();
+		},
+		'shift+tab': (event) => {
+			if (isNil(element)) return;
+			const activeElement = document.activeElement as HTMLElement;
+
+			const focusableElements = getFocusableElements(element);
+			const firstFocusableElement = focusableElements[0];
+			const lastFocusableElement = focusableElements[
+				focusableElements.length - 1
+			];
+
+			if (!element.contains(activeElement)) {
+				event.preventDefault();
+				if (notNil(lastFocusableElement)) {
+					lastFocusableElement.focus();
+
+					return;
+				}
+				activeElement?.blur();
+			}
+
+			if (activeElement !== firstFocusableElement) return;
+
+			event.preventDefault();
+			lastFocusableElement?.focus();
 		},
 	});
 
 	$effect(() => {
 		if (isVisible) {
 			document.addEventListener('keydown', onkeydown);
-			document.addEventListener('click', closeOnClickOutside);
+			document.addEventListener('pointerdown', closeOnClickOutside);
+			document.body.classList.add('overflow-hidden');
 		}
 
 		return () => {
 			document.removeEventListener('keydown', onkeydown);
-			document.removeEventListener('click', closeOnClickOutside);
+			document.removeEventListener('pointerdown', closeOnClickOutside);
+			document.body.classList.remove('overflow-hidden');
 		};
 	});
 </script>
 
 {@render trigger?.(setVisibility)}
 
-{#if isVisible}
+{#if isVisible && isBrowser}
 	<div
 		bind:this={element}
 		class="modal"
