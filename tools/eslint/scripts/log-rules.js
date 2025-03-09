398b05542ed1b16a51da8b90d4af53982002a682
Vadim Kotov <vadim@vadirn.io>
2025-03-09 16:56:57 +0500

    chore: add more rules to eslint

---
+++ tools/eslint/scripts/log-rules.js
@@ -0,0 +1,3 @@
+import eslint from '../src/index.js';
+
+console.dir(eslint, { depth: null });
--- tools/eslint/src/final.js
+++ tools/eslint/src/final.js
@@ -1,6 +1,6 @@
 import { setDefaultFiles } from './utils.js';
 
-export const final = () => setDefaultFiles({
+const finalRules = {
 	rules: {
 		'no-restricted-syntax': [
 			'error',
@@ -14,6 +14,15 @@ export const final = () => setDefaultFiles({
 				selector: 'CallExpression[callee.name="logAndReturn"]',
 				message: 'Not allowed in production code.',
 			},
+			{
+				selector: 'CallExpression[callee.property.name="forEach"]',
+				// eslint-disable-next-line @stylistic/max-len
+				message: 'Use for-of loops instead of forEach for better readability and performance',
+			},
+			{
+				selector: 'TSEnumDeclaration',
+				message: 'Use "as const" instead of enums',
+			},
 		],
 		'@typescript-eslint/no-empty-object-type': [
 			'error',
@@ -22,5 +31,121 @@ export const final = () => setDefaultFiles({
 			},
 		],
 		'prefer-const': ['error', { destructuring: 'all' }],
+		'array-callback-return': ['error', { allowImplicit: true }],
+		'complexity': ['error', { max: 10 }],
+		'max-depth': ['error', { max: 3 }],
+		'max-nested-callbacks': ['error', { max: 3 }],
+		'max-params': ['error', { max: 4 }],
+		'no-magic-numbers': ['error', { ignore: [0, 1, -1] }],
+		'@typescript-eslint/naming-convention': [
+			'error',
+			// Default - require strictCamelCase
+			{
+				selector: 'default',
+				format: ['strictCamelCase'],
+				leadingUnderscore: 'allowSingleOrDouble',
+			},
+
+			// Variables and parameters
+			{
+				selector: 'variable',
+				format: ['strictCamelCase', 'UPPER_CASE'],
+				leadingUnderscore: 'allowSingleOrDouble',
+			},
+
+			// Constants should be UPPER_CASE or strictCamelCase
+			{
+				selector: 'variable',
+				modifiers: ['const'],
+				format: ['strictCamelCase', 'UPPER_CASE', 'PascalCase'],
+				leadingUnderscore: 'allowSingleOrDouble',
+			},
+
+			// Boolean variables should use is/has/should prefix
+			{
+				selector: 'variable',
+				types: ['boolean'],
+				format: ['camelCase'],
+				prefix: ['is', 'has', 'should', 'can', 'did', 'will'],
+				leadingUnderscore: 'allowSingleOrDouble',
+				filter: {
+					regex: '^(required|disabled)?',
+					match: false,
+				},
+			},
+
+			// Array types should use plural names
+			{
+				selector: 'variable',
+				types: ['array'],
+				format: ['camelCase'],
+				suffix: ['s', 'List', 'Array', 'Collection'],
+				leadingUnderscore: 'allowSingleOrDouble',
+			},
+
+			// Classes and type aliases
+			{
+				selector: 'typeLike',
+				format: ['PascalCase'],
+			},
+
+			// Interface names must be prefixed with 'I'
+			{
+				selector: 'interface',
+				format: ['PascalCase'],
+				prefix: ['I'],
+				filter: {
+					regex: '^(Window)$',
+					match: false,
+				},
+			},
+
+			// Type parameters should use T prefix
+			{
+				selector: 'typeParameter',
+				format: ['PascalCase'],
+				prefix: ['T'],
+			},
+
+			{
+				selector: 'import',
+				format: ['strictCamelCase', 'PascalCase'],
+			},
+
+			// Object properties can be strictCamelCase or snake_case (for API responses)
+			{
+				selector: 'property',
+				format: ['strictCamelCase', 'snake_case'],
+				leadingUnderscore: 'allowSingleOrDouble',
+			},
+
+			// Readonly properties can be PascalCase, strictCamelCase, or snake_case
+			{
+				selector: 'objectLiteralProperty',
+				format: ['PascalCase', 'strictCamelCase', 'snake_case'],
+			},
+
+			// Function rules
+			{
+				selector: 'function',
+				format: ['strictCamelCase'],
+				leadingUnderscore: 'allowSingleOrDouble',
+			},
+
+			// Allow any format for quoted object properties
+			{
+				selector: 'property',
+				modifiers: ['requiresQuotes'],
+				format: null,
+			},
+
+			{
+				selector: 'objectLiteralProperty',
+				modifiers: ['requiresQuotes'],
+				format: null,
+			},
+		],
 	},
-});
+};
+
+export const final = () => setDefaultFiles(finalRules);
